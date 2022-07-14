918e7bedcd3dc97a33977d48254ecc7ae9048a85
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-07-14 14:06:32 -0400

    password generated

--- password_gen.py
+++ password_gen.py
@@ -3,7 +3,6 @@ import array
 
 #strong password generator: 12 character password using letters, numbers, and symbols
 def password_gen(max_length):
-    max_length= input ('Enter how many characters would you like your password to be: ')
     digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     lcase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h','i', 'j', 'k', 'm', 'n', 'o', 'p', 'q','r', 's', 't', 'u', 'v', 'w', 'x', 'y','z']
     ucase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y','Z']
@@ -15,8 +14,17 @@ def password_gen(max_length):
     rand_symbols = random.choice(symbols)
     temp = rand_digits + rand_lcase + rand_ucase + rand_symbols #temporary password with 4 characters, so as I generate the rest, I need to substract 4
     
-    for i in range(max_length - 4):
+    for x in range(max_length - 4):
         temp = temp + random.choice(char_list)    
+        temp_list = array.array('u', temp)
+        random.shuffle(temp_list)
+        password = ""
+    for x in temp_list:
+        password = password + x
+        
+    print("your password is: " + password)
     
+password_gen(8)
+            
 
 
